fb782d3e81d858054165d40b92c4f0e3b6c66242
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2022-01-14 16:16:28 -0300

    Initial commit

---
+++ src/list1/Exercise1.java
@@ -0,0 +1,16 @@
+package list1;
+
+public class Exercise1 {
+    int n1 = 0;
+    int n2 = 0;
+
+    public Exercise1(int n1, int n2) {
+        this.n1 = n1;
+        this.n2 = n2;
+    }
+
+    public void SUM(){
+        int sum = n1 + n2;
+        System.out.println("The sum between the number 1 and 2 is "+sum);
+    }
+}
